8e82fb819b439a7fb3d6200aa8c8c2576aa6e978
gabriele-salvai <gabriele.salvai@iismajoranamoncalieri.edu.it>
2024-12-04 12:43:22 +0000

    r

---
+++ somma.py
@@ -0,0 +1,12 @@
+n=-1
+while n<1:
+    n= float(input("inserisci n:\t"))
+else:
+    i=0
+    r=0
+    while i<=n:
+        r+=i
+        i+=1
+        print(r) 
+    else: 
+        print("ecco a te la somma dei primi {n} numeri")
